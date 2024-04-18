55caf4e9199ae6d9838b65ffa3b1f6692e251290
Fernando Júnior <contato@fernandojr.dev>
2024-04-18 15:02:56 -0300

    Update scripts

--- scripts/loadComponents.js
+++ scripts/loadComponents.js
@@ -1,14 +1,30 @@
-function loadComponent(url, elementId, eventName) {
-    fetch(url)
+document.addEventListener('DOMContentLoaded', function() {
+    // Carregar a Navbar
+    fetch('components/navbar.html')
         .then(response => response.text())
         .then(html => {
-            document.getElementById(elementId).innerHTML = html;
-            document.dispatchEvent(new CustomEvent(eventName)); // Disparar um evento após carregar
+            const navbarPlaceholder = document.getElementById('navbar-placeholder');
+            navbarPlaceholder.innerHTML = html;
+            updateNavbarLinks(); // Atualiza os links após carregar a navbar
         });
-}
 
-// Carregar navbar e footer com eventos customizados
-document.addEventListener('DOMContentLoaded', function() {
-    loadComponent('components/navbarComponent.html', 'navbar-placeholder', 'navbarLoaded');
-    loadComponent('components/footerComponent.html', 'footer-placeholder', 'footerLoaded');
+    // Carregar o Footer
+    fetch('components/footer.html')
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById('footer-placeholder').innerHTML = html;
+        });
 });
+
+function updateNavbarLinks() {
+    const currentPage = window.location.pathname.split('/').pop();
+    document.querySelectorAll('#navbar-placeholder a').forEach(link => {
+        if (link.getAttribute('href').includes(currentPage)) {
+            link.classList.add('active'); // Adiciona a classe 'active' se estiver na página atual
+        }
+        // Corrige os links para referenciar corretamente as seções na página inicial
+        if (link.hash && currentPage !== 'index.html') {
+            link.href = '/index.html' + link.hash;
+        }
+    });
+}
---
+++ scripts/scripts.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Suavização da rolagem para links internos
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop - document.querySelector('header').offsetHeight,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+});
