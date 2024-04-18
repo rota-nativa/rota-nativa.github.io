88b3d1c6cdaffc9ea9ffd19f596de90a652c3d54
Fernando Júnior <contato@fernandojr.dev>
2024-04-18 03:57:06 -0300

    Fix links on navbar

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,6 +1,6 @@
 function setupEventListeners() {
     // Pega todos os links que começam com '#'
-    const smoothScrollLinks = document.querySelectorAll('a[href^="#"]');
+    const smoothScrollLinks = document.querySelectorAll('a[href^="/#"]');
     const logoLink = document.querySelector('a.navbar-brand'); // Selecione a Logo se ela tiver a classe 'navbar-brand'
     const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
     const navbarCollapse = document.querySelector('.navbar-collapse');
@@ -8,14 +8,14 @@ function setupEventListeners() {
     smoothScrollLinks.forEach(link => {
         link.addEventListener('click', function (e) {
             e.preventDefault(); // Impede o comportamento padrão do link
-            if (this.getAttribute('href') === '#') {
+            if (this.getAttribute('href') === '/#') {
                 // Se o href é apenas '#', trate como um scroll para o topo
                 window.scrollTo({
                     top: 0, // Volta para o topo da página
                     behavior: 'smooth' // Efeito de scroll suave
                 });
             } else {
-                let target = document.querySelector(this.getAttribute('href')); // Pega o elemento destino
+                let target = document.querySelector(this.getAttribute('data-to')); // Pega o elemento destino
                 const navbarHeight = 60; // Altura da barra de navegação
 
                 // Calcula a posição necessária para o topo do elemento destino ficar visível
